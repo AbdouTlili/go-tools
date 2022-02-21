7accec595b8e1f39fe8269a320ef2deae808385c
TliliAbdou <abderrahmen.tlili@eurecom.fr>
2022-02-21 16:19:20 +0100

    updte deps

--- main/go.mod
+++ main/go.mod
@@ -2,4 +2,4 @@ module github.com/AbdouTlili/go-tools/main
 
 go 1.17
 
-replace github.com/AbdouTlili/go-tools/files => ../files
+
--- main/main.go
+++ main/main.go
@@ -1,5 +1,7 @@
 package main
 
-func main() {
+import "github.com/AbdouTlili/go-tools/files"
 
+func main() {
+	files.GetFloat("data.txt")
 }
